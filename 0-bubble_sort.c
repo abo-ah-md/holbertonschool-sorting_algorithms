a5a1a8b28c1170e7449a149d4e84026410111694
ismailrowaihy <ismailrowaihy@gmail.com>
2025-06-27 12:17:52 +0000

    added bubble sort algo

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
-void swap(int *from,int *to)
-{
-  int *temp;
-
-  temp = from;
-  from = to;
-  to = temp;
-  
-}
-
 /**
- *buble_sort - simple bubble sort algo
- *@array: array to sort 
+ *bubble_sort - simple bubble sort algo
+ *@array: array to sort
  *@size: size of the array
  *
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i,j,s = size;
-  /*int check_swap = 0;*/
-  int temp;
-  
-  for (i = 0; i < s; i++)
-    {
+size_t i, j;
+int check_swap;
+int temp;
 
-	
-      for (j = 0 ; j < s - i - 1; j++)
-	{
-         if (array[j] > array[j+1])
-	   {
-	     temp = array[j];
-	     array[j] = array[j+1];
-	     array[j+1] = temp;
+for (i = 0; i < size - 1; i++)
+{
 
-	     /* check_swap = 1;*/
-	     print_array(array,size);
-	   }
-        }
-     }
+check_swap = 0;
+
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+
+check_swap = 1;
+print_array(array, size);
+}
+}
+
+if (check_swap == 0)
+break;
+
+}
 }
