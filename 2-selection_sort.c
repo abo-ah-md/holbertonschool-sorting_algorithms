fbdfee964fe5f3674db106b67e95f53c47f0ccc3
ismailrowaihy <ismailrowaihy@gmail.com>
2025-06-28 11:32:49 +0000

    added selection sort algo

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ *selection_sort - simple selection sorting algo
+ *@array: array to sort
+ *@size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+unsigned int i, j;
+int min, temp = 0;
+
+for (i = 0; i < size - 1; i++)
+{
+min = i;
+
+for (j = i + 1; j < size; j++)
+{
+
+if (array[j] < array[min])
+min = j;
+}
+
+temp = array[i];
+array[i] = array[min];
+array[min] = temp;
+
+print_array(array, size);
+}
+}
